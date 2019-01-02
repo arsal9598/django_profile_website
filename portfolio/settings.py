a70de679013590b00ba6cee4bd6b9e03ba7235cd
techne <you@example.com>
2019-01-01 20:32:17 -0600

    please stop

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = True
 
 ALLOWED_HOSTS = ['arsal-khan.herokuapp.com', '127.0.0.1']
 
